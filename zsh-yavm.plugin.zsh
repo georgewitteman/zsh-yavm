f0d3abb404165ff97a13bfd6d2f0dab980c0f135
George Witteman <george@witteman.me>
2020-07-06 20:43:05 -0700

    Cleanup

--- zsh-yavm.plugin.zsh
+++ zsh-yavm.plugin.zsh
@@ -3,24 +3,4 @@
 fpath+=("${0:a:h}/autoload/")
 autoload -Uz "${0:a:h}"/autoload/*
 
-function yavm() {
-  local cmd="$1"
-
-  if [[ -z "$cmd" ]]; then
-    yavm_help
-    return "$status"
-  fi
-
-  shift
-
-  local sub_script="yavm_${cmd/\-/_}"
-
-  if (( ! $+functions[$sub_script] )); then
-    _yavm_invalid_command "$cmd"
-    return "$status"
-  fi
-
-  "$sub_script" "$@"
-}
-
 preexec_functions+=(yavm_set_path)
