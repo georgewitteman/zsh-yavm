a46a49e9881677d985fef83e824b970f166a00e1
George Witteman <george@witteman.me>
2020-07-14 16:34:39 -0700

    Updates

--- zsh-yavm.plugin.zsh
+++ zsh-yavm.plugin.zsh
@@ -2,7 +2,13 @@
 
 YAVM_PLUGIN_DIR="${0:a:h}/plugins"
 YAVM_CONFIG_DIR="${YAVM_CONFIG_DIR:-${XDG_CONFIG_HOME:-$HOME/.config}/yavm}"
-YAVM_INSTALL_DIR="${YAVM_INSTALL_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/yavm/installs}"
+YAVM_DATA_DIR="${YAVM_INSTALL_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/yavm}"
+YAVM_INSTALL_DIR="${YAVM_DATA_DIR}/installs}"
+
+# echo "YAVM_PLUGIN_DIR: $YAVM_PLUGIN_DIR"
+# echo "YAVM_CONFIG_DIR: $YAVM_CONFIG_DIR"
+# echo "YAVM_DATA_DIR: $YAVM_DATA_DIR"
+# echo "YAVM_INSTALL_DIR: $YAVM_INSTALL_DIR"
 
 fpath+=("${0:a:h}/autoload")
 autoload -Uz "${0:a:h}"/autoload/*
